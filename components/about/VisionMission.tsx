d70db0b582f5db34d109bd5499b7dbc3aa102b91
petranusa-id <mn29075@gmail.com>
2026-08-04 13:55:23 +0700

    Create VisionMission.tsx

---
+++ components/about/VisionMission.tsx
@@ -0,0 +1,48 @@
+export default function VisionMission() {
+  return (
+    <section className="bg-slate-100 py-24">
+
+      <div className="max-w-7xl mx-auto grid lg:grid-cols-2 gap-10 px-6">
+
+        <div className="bg-white rounded-3xl p-10 shadow">
+
+          <h3 className="text-3xl font-bold text-green-700">
+            Vision
+          </h3>
+
+          <p className="mt-6 leading-9 text-gray-600">
+
+            Provide effective,
+            competitive,
+            and practical business
+            solutions to partners
+            in order to optimize
+            operational efficiency
+            and maximize profits.
+
+          </p>
+
+        </div>
+
+        <div className="bg-white rounded-3xl p-10 shadow">
+
+          <h3 className="text-3xl font-bold text-green-700">
+            Mission
+          </h3>
+
+          <p className="mt-6 leading-9 text-gray-600">
+
+            To become a trusted and leading
+            partner in professional training
+            and certification while delivering
+            measurable and sustainable results.
+
+          </p>
+
+        </div>
+
+      </div>
+
+    </section>
+  );
+}
