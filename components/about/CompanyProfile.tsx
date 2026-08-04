99da0234620b1bb662819c4909d240d5808767b8
petranusa-id <mn29075@gmail.com>
2026-08-04 13:54:47 +0700

    Create CompanyProfile.tsx

---
+++ components/about/CompanyProfile.tsx
@@ -0,0 +1,32 @@
+export default function CompanyProfile() {
+  return (
+    <section className="py-24">
+
+      <div className="max-w-7xl mx-auto px-6">
+
+        <h2 className="text-5xl font-bold">
+          Company Profile
+        </h2>
+
+        <p className="mt-8 text-lg leading-9 text-gray-600">
+
+          PT. Petratama Abdi Nusa established a professional
+          legal entity in 2003 based in Jakarta, Indonesia.
+
+          Due to the high demand for skilled professionals
+          in the Indonesian energy sector, there is a vast
+          growing need for resource development.
+
+          Petranusa contributes by providing professional
+          Certification,
+          In House Training,
+          Consultancy,
+          and Public Courses.
+
+        </p>
+
+      </div>
+
+    </section>
+  );
+}
