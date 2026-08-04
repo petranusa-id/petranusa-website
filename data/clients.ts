2df7c9bcebc37c42310adc8d398073bbe57fa7ac
petranusa-id <mn29075@gmail.com>
2026-08-04 13:46:43 +0700

    Update clients.ts

--- data/clients.ts
+++ data/clients.ts
@@ -1,12 +1,23 @@
 export const clients = [
-  "/clients/bp.png",
-  "/clients/chevron.png",
-  "/clients/eni.png",
-  "/clients/exxonmobil.png",
-  "/clients/kufpec.png",
-  "/clients/medco.png",
-  "/clients/pertamina.png",
-  "/clients/phr.png",
-  "/clients/skkmigas.png",
-  "/clients/pii.png",
+
+"/clients/bp.png",
+
+"/clients/chevron.png",
+
+"/clients/exxonmobil.png",
+
+"/clients/eni.png",
+
+"/clients/medco.png",
+
+"/clients/pertamina.png",
+
+"/clients/phr.png",
+
+"/clients/skkmigas.png",
+
+"/clients/pii.png",
+
+"/clients/kufpec.png",
+
 ];
