e929558e51ec63043149b1190798b7c24ab1b40d
petranusa-id <mn29075@gmail.com>
2026-08-04 14:39:33 +0700

    Create page.tsx

---
+++ app/news/[slug]/page.tsx
@@ -0,0 +1,54 @@
+import { notFound } from "next/navigation";
+import { news } from "@/data/news";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export function generateStaticParams() {
+  return news.map((item) => ({
+    slug: item.slug,
+  }));
+}
+
+export default function NewsDetailPage({
+  params,
+}: Props) {
+  const article = news.find(
+    (item) => item.slug === params.slug
+  );
+
+  if (!article) {
+    notFound();
+  }
+
+  return (
+    <main className="py-24">
+
+      <div className="max-w-4xl mx-auto px-6">
+
+        <h1 className="text-5xl font-bold">
+
+          {article.title}
+
+        </h1>
+
+        <p className="text-gray-500 mt-4">
+
+          {article.date}
+
+        </p>
+
+        <p className="mt-10 leading-9">
+
+          {article.excerpt}
+
+        </p>
+
+      </div>
+
+    </main>
+  );
+}
