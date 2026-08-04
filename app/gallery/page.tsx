85e63516805abbff812902ef2b60742ff7ea31da
petranusa-id <mn29075@gmail.com>
2026-08-04 14:10:01 +0700

    Create page.tsx

---
+++ app/gallery/page.tsx
@@ -0,0 +1,13 @@
+import Gallery from "@/components/home/Gallery";
+
+export const metadata = {
+  title: "Gallery",
+};
+
+export default function GalleryPage() {
+  return (
+    <main>
+      <Gallery />
+    </main>
+  );
+}
