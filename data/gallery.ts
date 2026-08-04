79938af497e72353d59cfcf9facf7ce99182b75c
petranusa-id <mn29075@gmail.com>
2026-08-04 13:48:23 +0700

    Update gallery.ts

--- data/gallery.ts
+++ data/gallery.ts
@@ -1,8 +1,15 @@
 export const gallery = [
-  "/gallery/1.jpg",
-  "/gallery/2.jpg",
-  "/gallery/3.jpg",
-  "/gallery/4.jpg",
-  "/gallery/5.jpg",
-  "/gallery/6.jpg",
+
+"/gallery/1.jpg",
+
+"/gallery/2.jpg",
+
+"/gallery/3.jpg",
+
+"/gallery/4.jpg",
+
+"/gallery/5.jpg",
+
+"/gallery/6.jpg",
+
 ];
