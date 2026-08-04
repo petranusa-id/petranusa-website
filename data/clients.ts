0d5a6bd6063f4ddd5e1131b1a28a0ac14bf14a2c
petranusa-id <mn29075@gmail.com>
2026-08-04 12:31:05 +0700

    Create clients.ts

---
+++ data/clients.ts
@@ -0,0 +1,12 @@
+export const clients = [
+  "/clients/bp.png",
+  "/clients/chevron.png",
+  "/clients/eni.png",
+  "/clients/exxonmobil.png",
+  "/clients/kufpec.png",
+  "/clients/medco.png",
+  "/clients/pertamina.png",
+  "/clients/phr.png",
+  "/clients/skkmigas.png",
+  "/clients/pii.png",
+];
