1ab7b264b6f6ee18b101c475c5609b1774ef6660
petranusa-id <mn29075@gmail.com>
2026-08-04 14:41:13 +0700

    Create site.ts

---
+++ lib/site.ts
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  name: "PT. Petratama Abdi Nusa",
+
+  url: "https://petranusa.my.id",
+
+  email: "pan@petranusa.co.id",
+
+  phone: "+62 21 7919 7673",
+
+  instagram: "https://instagram.com/petranusa.id",
+
+  address: {
+    office1:
+      "Jl. Komplek Rukan Buncit Mas BB-3, Duren Tiga, Jakarta Selatan",
+    office2:
+      "Jl. Mimosa I Blok A5, Buncit Indah, Pasar Minggu, Jakarta Selatan",
+  },
+};
