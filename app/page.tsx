66b48d1fbe57994c0fa9281573f9ed99511a44ec
petranusa-id <mn29075@gmail.com>
2026-08-04 12:39:39 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,3 +9,22 @@ export default function Home() {
     </main>
   );
 }
+import Hero from "@/components/home/Hero";
+import About from "@/components/home/About";
+import Services from "@/components/home/Services";
+import Statistics from "@/components/home/Statistics";
+import Clients from "@/components/home/Clients";
+import Gallery from "@/components/home/Gallery";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Services />
+      <Statistics />
+      <Clients />
+      <Gallery />
+    </>
+  );
+}
