8ccd11293c72f1570dd69a9e5f8e2e350201dc21
petranusa-id <mn29075@gmail.com>
2026-08-04 14:34:11 +0700

    Create news.ts

---
+++ data/news.ts
@@ -0,0 +1,13 @@
+import { News } from "@/types/news";
+
+export const news: News[] = [
+  {
+    id: 1,
+    title: "New Professional Training Program",
+    slug: "new-training-program",
+    image: "/images/news.jpg",
+    date: "2026-08-01",
+    excerpt:
+      "Petranusa launches new competency development programs.",
+  },
+];
