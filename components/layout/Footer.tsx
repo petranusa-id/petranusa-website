93c45dbf7a353a96db70a162803740230275f26c
petranusa-id <mn29075@gmail.com>
2026-08-04 12:25:30 +0700

    Create Footer.tsx

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,25 @@
+PT. Petratama Abdi Nusa
+
+Office I
+
+Office II
+
+Quick Links
+
+Certification
+
+Training
+
+Consultancy
+
+Public Course
+
+Email
+
+pan@petranusa.co.id
+
+Instagram
+
+@petranusa.id
+
+Copyright © 2026
